523142a9c93b708e0a13614de695baa34e075420
Muhammad Akbarul Ihsan <barullisan@gmail.com>
2022-11-05 15:04:44 +0700

    Selesai penambahan Kdeluxe,Kdeluxesingle, Main

---
+++ Mainhotel.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Mainhotel {
+    public static void main(String[] args) {
+        Scanner In = new Scanner(System.in);
+
+        int pilihan = In.nextInt();
+        switch(pilihan){
+            case 1:
+            break;
+
+            case 2:
+            break;
+
+            case 3:
+            KamarDeluxe KD = new KamarDeluxe();
+            break;
+
+            case 4:
+            break;
+
+            case 5:
+            break;
+        }
+
+    }
+}
