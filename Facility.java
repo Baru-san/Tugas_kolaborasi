b376e088d88d9831fbfe567841e57be29e2e97ed
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-11-05 11:42:02 +0700

    add facility

---
+++ Facility.java
@@ -0,0 +1,42 @@
+public class Facility {
+    private boolean AC;
+    private boolean pool;
+    private boolean breakfast;
+    private boolean dinner;
+    private boolean wifi;
+
+    public Facility(boolean AC, boolean pool, boolean breakfast,
+    boolean dinner, boolean wifi)
+    {
+        this.AC=AC;
+        this.pool=pool;
+        this.breakfast=breakfast;
+        this.dinner=dinner;
+        this.wifi=wifi;
+    }
+    
+    public boolean getAC()
+    {
+        return this.AC;
+    }
+
+    public boolean getPool()
+    {
+        return this.pool;
+    }
+
+    public boolean getBreakfast()
+    {
+        return this.breakfast;
+    }
+
+    public boolean getDinner()
+    {
+        return this.dinner;
+    }
+
+    public boolean getWifi()
+    {
+        return this.wifi;
+    }
+}
