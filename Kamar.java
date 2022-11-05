7cf780ed0e89a0eda4b6135a132011cfd89608d3
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-11-05 11:28:41 +0700

    Update Kamar.java

--- Kamar.java
+++ Kamar.java
@@ -6,8 +6,6 @@ public abstract class Kamar
 {
     public abstract void booking();
     public abstract String getDescription();
-    public abstract int roomAvailable();
-    public abstract int dayAvailable();
     public abstract int getPrice();
     public abstract int getRating();
 }
